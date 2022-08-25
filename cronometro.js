f9d687b67701f191e543e0727c216c9b9f504257
CaioMDutra <caimcd.dutra@hotmail.com>
2022-08-25 15:12:30 -0300

    Cronometro finalizado

---
+++ cronometro.js
@@ -0,0 +1,44 @@
+let seconds = 0;
+let minuts = 0;
+let hours = 0;
+let interval;
+
+function twonum(digits){
+    if(digits<10){
+        return("0"+digits)
+    }else{
+        return(digits)
+    }
+    
+}
+
+function start(){
+    counter()
+    interval=setInterval(counter,1000)
+}
+
+function pause(){
+    clearInterval(interval)
+}
+
+function stop(){
+    clearInterval(interval)
+    minuts=0
+    seconds=0
+    document.getElementById("watch").innerText ="00:00"
+}
+
+function counter(){
+    seconds++
+    if(seconds==60){
+        minuts++
+        seconds=0
+        if(minuts==60){
+            hours++
+            minuts=0
+
+        }
+
+    }
+    document.getElementById("watch").innerText =twonum(hours)+":"+twonum(minuts)+":"+twonum(seconds)
+}
